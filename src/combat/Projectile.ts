c7fb8321dc464be2c22eec2a71efd1e27bab3968
umaydie-cyber <umaydie@icloud.com>
2026-07-17 14:18:37 +0800

    fix: make class selection cards clickable

--- src/combat/Projectile.ts
+++ src/combat/Projectile.ts
@@ -1,5 +1,6 @@
 import Phaser from 'phaser';
 export class Projectile extends Phaser.Physics.Arcade.Sprite {
   damage: number;
-  constructor(scene: Phaser.Scene,x:number,y:number,damage:number) { super(scene,x,y,'flame'); scene.add.existing(this); scene.physics.add.existing(this); this.damage=damage; this.setCircle(10).setDepth(4); }
+  slow = 0;
+  constructor(scene: Phaser.Scene,x:number,y:number,damage:number,texture='flame') { super(scene,x,y,texture); scene.add.existing(this); scene.physics.add.existing(this); this.damage=damage; this.setCircle(10).setDepth(4); }
 }
